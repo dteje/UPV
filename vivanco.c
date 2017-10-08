a36e1aeb897d377d9f24d876205cc0b16d5eea2b
dteje <daniel.tejedor@outlook.com>
2017-10-08 14:20:08 +0200

    Autosave

--- vivanco.c
+++ vivanco.c
@@ -1,28 +1,11 @@
-#define PI = 3.14159
+#include <stdio.h>
+#include <math.h>
 
-int main(){
-	float radio, altura;
-	float area_b, area_l, vol;
-	
-	printf("Radio de la base (en cm):");
-	scanf("%f", &radio);
-	printf("\n");
-	
-	printf("Altura (en cm):")
-	scanf("%f", &altura);
-	printf("\n");
+float PI = 3.14159;
 
-	area_b = area_base(radio);
-	area_l = area_lateral(altura, radio);
-	vol = volumen(altura, radio);
-
-	printf("Área de la base = %.2f cm2", area_b);
-	printf("Área lateral = %.2f cm2", area_l);
-	printf("Volumen del cono = %.2f cm3", vol);
-}
 
 float area_base(float r){
-	float a = PI * r * r;
+	float a = (PI * r * r);
 	return a;
 }
 
@@ -37,3 +20,27 @@ float volumen(float h, float r){
 	float v = (PI * r * r * h)/3;	
 	return v;
 }
+
+int main(){
+	float radio, altura;
+	float area_b, area_l, vol;
+	
+	printf("Radio de la base (en cm):");
+	scanf("%f", &radio);
+	//printf("\n");
+	
+	printf("Altura (en cm):");
+	scanf("%f", &altura);
+	printf("\n");
+
+	area_b = area_base(radio);
+	area_l = area_lateral(altura, radio);
+	vol = volumen(altura, radio);
+
+	printf("Área de la base = %.2f cm2\n", area_b);
+	printf("Área lateral = %.2f cm2\n", area_l);
+	printf("Volumen del cono = %.2f cm3\n", vol);
+
+	return 0;
+}
+
